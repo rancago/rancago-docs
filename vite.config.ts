476ae763cb6d3ccf3988882889bd6c1feac2f810
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2026-07-29 00:56:08 +0700

    ci: fix pages deploy (bun)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+  const base =
+    process.env.GITHUB_PAGES === 'true' && repo.length > 0 ? `/${repo}/` : '/';
   return {
+    base,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
